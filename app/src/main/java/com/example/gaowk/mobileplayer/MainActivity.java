1c3298017e27b466e9563109b8e3ed25eba084ff
GaoWK <1012295514@qq.com>
2019-01-22 12:37:19 +0800

    进入MainActivity  设置启动模式

---
+++ app/src/main/java/com/example/gaowk/mobileplayer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gaowk.mobileplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by GaoWK on 2019/1/22.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/gaowk/mobileplayer/SplashActivity.java
+++ app/src/main/java/com/example/gaowk/mobileplayer/SplashActivity.java
@@ -1,14 +1,42 @@
 package com.example.gaowk.mobileplayer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SplashActivity extends Activity {
 
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //在主线程中执行
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
